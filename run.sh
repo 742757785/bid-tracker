fe3f821e6b26a8515e096b10e44c31ba409b27da
742757785 <103161173+742757785@users.noreply.github.com>
2026-05-06 11:32:41 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-npm init -y
-npm install node-fetch cheerio
-node fetch-data.js
-git config user.email "action@github.com"
-git config user.name "Action"
-git add data.json
-git commit -m "update" || true
-git push
